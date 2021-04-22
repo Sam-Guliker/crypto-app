972bdb1abc2f609dec09a1939916af414a65c189
Sam-Guliker <samguliker@hotmail.com>
2021-04-22 21:46:15 +0200

    FIXED IT WITH REACT HOOKS LETS GO.

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -4,8 +4,8 @@ import {useState, useEffect} from "react";
 import './Container.scss'
 
 export default function App() {
-   const [data, setData] = useState([]) 
-   const [userInput, searchInput] = useState('')
+    const [data, setData] = useState([]) 
+    const [userInput, searchInput] = useState('')
 
     useEffect(()=> {
         fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false")
@@ -18,56 +18,53 @@ export default function App() {
     const handleUserInput = (e) => {
         searchInput(e.target.value)
     }
-        return (
-            <div className="container">
-               <h1>A crypto currency tracker</h1>
-                <input 
-                        className="searchbar" 
-                        value={userInput} 
-                        type="text" 
-                        name="search" 
-                        onChange={handleUserInput} 
-                        placeholder="Search a currency"
-                 />
 
-                {
-                <ul className="crypto-list">
-                    {data.map((coin, key) => {
+    return (
+        <div className="container">
+            <h1>A crypto currency tracker</h1>
+            <input 
+                    className="searchbar" 
+                    value={userInput} 
+                    type="text" 
+                    name="search" 
+                    onChange={handleUserInput} 
+                    placeholder="Search a currency"
+                />
+
+            {/* {
+            <ul className="crypto-list">
+                {data.map((coin, key) => {
+                    return (
+                        <li className="crypto-item" key={key}>
+                            <div className="crypto-group-1">
+                                <img className="crypto-icon" src={coin.image} />
+                                <p className="crypto-name">{coin.id}({coin.symbol})</p>
+                            </div>
+                            <p className="crypto-price"> Price: €{coin.current_price}</p>
+                        </li>
+                    )
+                })}
+            </ul>
+            } */}
+            
+            <ul className="crypto-list">
+                { data.filter((val) => {
+                        if (val.id.toLowerCase().includes(userInput.toLowerCase())) {
+                            return val
+                        }
+                    }).map((val, key) => {
                         return (
-                            <li className="crypto-item" key={key}>
+                            <li key={key} className="crypto-item">
                                 <div className="crypto-group-1">
-                                    <img className="crypto-icon" src={coin.image} />
-                                    <p className="crypto-name">{coin.id}({coin.symbol})</p>
+                                    <img className="crypto-icon" src={val.image} />
+                                    <p className="crypto-name">{val.id}({val.symbol})</p>
                                 </div>
-                                <p className="crypto-price"> Price: €{coin.current_price}</p>
+                                <p className="crypto-price"> Price: €{val.current_price}</p>
                             </li>
                         )
-                    })}
-                </ul>
+                    })
                 }
-                
-                {/* <ul className="crypto-list">
-                    { this.state.data.filter((val) => {
-                            if (this.state.filterData = '') {
-                                return val
-                            } else if (val.id.toLowerCase().includes(this.state.userInput.toLowerCase())) {
-                                return val
-                            }
-                        }).map((val, key) => {
-                            return (
-                                <li key={key} className="crypto-item">
-                                    <div className="crypto-group-1">
-                                        <img className="crypto-icon" src={val.image} />
-                                        <p className="crypto-name">{val.id}({val.symbol})</p>
-                                    </div>
-                                    <p className="crypto-price"> Price: €{val.current_price}</p>
-                                </li>
-                            )
-                        })
-                    }
-
-                    {console.log(this.state.userInput)}
-                </ul> */}
-            </div>
-        )
+            </ul>
+        </div>
+    )
 }
