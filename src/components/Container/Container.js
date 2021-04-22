cd116bf016bab8347a9c1b584052552441b5c835
Sam-Guliker <samguliker@hotmail.com>
2021-04-22 21:40:30 +0200

    Render list

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,47 +3,21 @@ import {useState, useEffect} from "react";
 
 import './Container.scss'
 
-// import Searchbar from "../Searchbar/Searchbar.js"
-
 export default function App() {
    const [data, setData] = useState([]) 
    const [userInput, searchInput] = useState('')
 
-    // this.handleChange = this.handleChange.bind(this);
-    useEffect (()=> {
+    useEffect(()=> {
         fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false")
             .then(response => response.json())
             .then(response => setData(response))
             .then(console.log(data))
         },[])
 
-    // componentWillMount () {
-    //   
-    // }
-
-    // handleChange(event){
-    //     let filter = event.target.value;
-    //     this.setState({userInput: filter})
-
-    //     let filteredData = this.state.data.filter((coin) => {
-    //         if(filter === coin.id) {
-    //             return coin
-    //         }
-    //     })
-
-    //     this.setState({ filterData: filteredData})
-    // }
 
     const handleUserInput = (e) => {
         searchInput(e.target.value)
-
-        // let filteredData = this.state.data.filter((coin) => {
-        //     if(filter === coin.id) {
-        //         return coin
-        //     }
-        // })
     }
-
         return (
             <div className="container">
                <h1>A crypto currency tracker</h1>
@@ -55,6 +29,22 @@ export default function App() {
                         onChange={handleUserInput} 
                         placeholder="Search a currency"
                  />
+
+                {
+                <ul className="crypto-list">
+                    {data.map((coin, key) => {
+                        return (
+                            <li className="crypto-item" key={key}>
+                                <div className="crypto-group-1">
+                                    <img className="crypto-icon" src={coin.image} />
+                                    <p className="crypto-name">{coin.id}({coin.symbol})</p>
+                                </div>
+                                <p className="crypto-price"> Price: €{coin.current_price}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
+                }
                 
                 {/* <ul className="crypto-list">
                     { this.state.data.filter((val) => {
