507b0d1799882e03a1a414f80061675c692bcd79
Sam-Guliker <samguliker@hotmail.com>
2021-05-04 23:16:45 +0200

    star animation and css fixes on navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import {ReactComponent as Search} from '../assets/search.svg';
 
 export default function Header({setUserInput, userInput, header}) {
+    
     return (
         <header className="header">
             <h1 className="heading-01">{header}</h1>
